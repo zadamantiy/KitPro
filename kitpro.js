1efda73c8f95b355a2e2032b5a8c8daadcd5317c
zadamantiy <33583385+zadamantiy@users.noreply.github.com>
2021-01-04 23:58:39 +0300

    Update kitpro.js

--- kitpro.js
+++ kitpro.js
@@ -1,11 +1,15 @@
-var button = document.createElement("button");
-button.innerHTML = "Do Something";
+var img = document.createElement("img");
+img.src = "http://vpngw.avalon.ru:23501/ru/figureimageru?n=64&pid=88";
+img.width = "40"
+img.height = "40"
+img.style.margin = "10px"
 
-// 2. Append somewhere
+var a=document.createElement('a');
+a.href='https://vk.com/mkitsaish';
+a.appendChild(img);
+
 var body = document.getElementsByClassName("page_header")[0];
-body.appendChild(button);
+body.align = "center"
+body.appendChild(a);
 
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("did something");
-});
+System.import('https://raw.githubusercontent.com/zadamantiy/KitPro/main/kitpro.js');
